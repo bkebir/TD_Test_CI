55deb266205315852c314033e720dae9c17f087d
bkebir <bkebir@iut.univ-paris8.fr>
2023-03-06 13:47:40 +0100

    création classe CartePizzeria

---
+++ CartePizzeria.py
@@ -0,0 +1,22 @@
+class CartePizzeria:
+    def __init__(self):
+        self.pizzas = []
+
+    def is_empty(self):
+        return len(self.pizzas) == 0
+
+    def nb_pizzas(self):
+        return len(self.pizzas)
+    
+    def add_pizza(self, pizza):
+        self.pizzas.append(pizza)
+    
+    def remove_pizza(self, name):
+        for pizza in self.pizzas:
+            if pizza.name == name:
+                self.pizzas.remmove(pizza)
+                return
+            raise CartePizzeriaException("La pizza n'est pas trouvée")
+
+class CartePizzeriaException(Exception):
+    pass
